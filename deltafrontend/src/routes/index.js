0436005ce2981ab2b054fce5d2c535854670853a
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-20 17:00:16 -0300

    Adicionando Framework Material-UI para o FrontEnd, adicionando React-Router-Dom para as rotas do frontEnd e Criando a tela principal Main

---
+++ deltafrontend/src/routes/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
+import Main from "../pages/Main";
+
+
+const Routes = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Main} />
+        </Switch>
+    </Router>
+);
+
+export default Routes;
