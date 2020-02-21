fe1e20d217914922c5f52366aa1ea682bbcb09d5
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-20 22:52:04 -0300

    Alterando css e estrutura da Lista de Exibicao do aluno

--- deltafrontend/src/components/Tabela.js
+++ deltafrontend/src/components/Tabela.js
@@ -10,7 +10,7 @@ import Paper from '@material-ui/core/Paper';
 
 const StyledTableCell = withStyles(theme => ({
   head: {
-    backgroundColor: 'blue',
+    backgroundColor: '#EB5160',
     color: theme.palette.common.white,
   },
   body: {
@@ -31,16 +31,16 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0),
+  createData('Ice cream sandwich', 237, 9.0),
+  createData('Eclair', 262, 16.0),
+  createData('Cupcake', 305, 3.7),
+  createData('Gingerbread', 356, 16.0)
 ];
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 700,
+    minWidth: 600,
   },
 });
 
@@ -52,11 +52,9 @@ export default function CustomizedTables() {
       <Table className={classes.table} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            <StyledTableCell>Nome</StyledTableCell>
+            <StyledTableCell>Endereço</StyledTableCell>
+            <StyledTableCell>Foto</StyledTableCell>            
           </TableRow>
         </TableHead>
         <TableBody>
@@ -65,10 +63,8 @@ export default function CustomizedTables() {
               <StyledTableCell component="th" scope="row">
                 {row.name}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+              <StyledTableCell>{row.calories}</StyledTableCell>
+              <StyledTableCell>{row.fat}</StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
