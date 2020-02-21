34937d79d276f7cc0c12ee85a484dfcaf983d3b1
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-21 15:42:00 -0300

    Criando minha Controller Aluno.php

--- deltabackend/application/controllers/Aluno.php
+++ deltabackend/application/controllers/Aluno.php
@@ -1,9 +1,8 @@
 <?php
 
-use deltabackend\application\libraries\REST_Controller;
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+//Header
 header('Access-Control-Allow-Origin: *');
 
 if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
@@ -11,51 +10,59 @@ if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 	header('Access-Control-Allow-Headers: Content-Type');
 	exit;
 }
-
 require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
 
-class WebsiteRestController extends REST_Controller {
-	
-	use REST_Controller {
-		REST_Controller::__construct as private __resTraitConstruct;
-	}
-	
-	function __construct() {
-        parent::__construct();
-		$this->__resTraitConstruct();
-		$this->load->model('website_model', 'wm');
-    }
-	
-	function websites_get() {
-        $websites = $this->wm->get_website_list();
+class Aluno extends REST_Controller {
+    
 
-        if ($websites) {
-            $this->response($websites, 200);
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+		$this->load->model('aluno', 'wm');
+        // Configure limits on our controller methods
+        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+        $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
+        $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
+
+        //Meus limites dos meus controles
+        $this->methods['aluno_get']['limit'] = 500; // 500 requests per hour per user/key        
+        $this->methods['add_aluno_post']['limit'] = 100; // 100 requests per hour per user/key
+        $this->methods['add_aluno_put']['limit'] = 50; // 50 requests per hour per user/key
+        $this->methods['delete_aluno_delete']['limit'] = 50; // 50 requests per hour per user/key
+        
+    }
+
+    function alunos_get() {
+        $alunos = $this->wm->get_aluno_list();
+
+        if ($alunos) {
+            $this->response($alunos, 200);
         } else {
             $this->response(array(), 200);
         }
     }
 
-    function website_get() {
-        if (!$this->get('id')) { //query parameter, example, websites?id=1
+    function aluno_get() {
+        if (!$this->get('id')) { //query parameter, example, alunos?id=1
             $this->response(NULL, 400);
         }
 
-        $website = $this->wm->get_website($this->get('id'));
+        $aluno = $this->wm->get_aluno($this->get('id'));
 
-        if ($website) {
-            $this->response($website, 200); // 200 being the HTTP response code
+        if ($aluno) {
+            $this->response($aluno, 200); // 200 being the HTTP response code
         } else {
             $this->response(array(), 500);
         }
     }
-	
-	function add_website_post() {
-        $website_title = $this->post('title');
-        $website_url = $this->post('url');
+
+    function add_aluno_post() {
+        $aluno_name = $this->post('name');
         
-        $result = $this->wm->add_website($website_title, $website_url);
+        $result = $this->wm->add_aluno($aluno_name);
 
         if ($result === FALSE) {
             $this->response(array('status' => 'failed'));
@@ -64,23 +71,11 @@ class WebsiteRestController extends REST_Controller {
         }
     }
 
-    function update_website_put() {
-        $website_id = $this->put('id');
-        $website_title = $this->put('title');
-        $website_url = $this->put('url');
+    function update_aluno_put() {
+        $aluno_id = $this->put('id');
+        $aluno_name = $this->put('name');
 
-        $result = $this->wm->update_website($website_id, $website_title, $website_url);
-
-        if ($result === FALSE) {
-            $this->response(array('status' => 'failed'));
-        } else {
-            $this->response(array('status' => 'success'));
-        }
-    }
-	
-	function delete_website_delete($website_id) { //path parameter, example, /delete/1
-
-        $result = $this->wm->delete_website($website_id);
+        $result = $this->wm->update_aluno($aluno_id, $aluno_name);
 
         if ($result === FALSE) {
             $this->response(array('status' => 'failed'));
@@ -88,5 +83,126 @@ class WebsiteRestController extends REST_Controller {
             $this->response(array('status' => 'success'));
         }
     }
-	
+
+    function delete_aluno_delete($aluno_id) { //path parameter, example, /delete/1
+
+        $result = $this->wm->delete_aluno($aluno_id);
+
+        if ($result === FALSE) {
+            $this->response(array('status' => 'failed'));
+        } else {
+            $this->response(array('status' => 'success'));
+        }
+    }
+    public function users_get()
+    {
+        // Users from a data store e.g. database
+        $users = [
+            ['id' => 1, 'name' => 'John', 'email' => 'john@example.com', 'fact' => 'Loves coding'],
+            ['id' => 2, 'name' => 'Jim', 'email' => 'jim@example.com', 'fact' => 'Developed on CodeIgniter'],
+            ['id' => 3, 'name' => 'Jane', 'email' => 'jane@example.com', 'fact' => 'Lives in the USA', ['hobbies' => ['guitar', 'cycling']]],
+        ];
+
+        $id = $this->get('id');
+
+        // If the id parameter doesn't exist return all the users
+
+        if ($id === NULL)
+        {
+            // Check if the users data store contains users (in case the database result returns NULL)
+            if ($users)
+            {
+                // Set the response and exit
+                $this->response($users, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+            }
+            else
+            {
+                // Set the response and exit
+                $this->response([
+                    'status' => FALSE,
+                    'message' => 'No users were found'
+                ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+            }
+        }
+
+        // Find and return a single record for a particular user.
+
+        $id = (int) $id;
+
+        // Validate the id.
+        if ($id <= 0)
+        {
+            // Invalid id, set the response and exit.
+            $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        }
+
+        // Get the user from the array, using the id as key for retrieval.
+        // Usually a model is to be used for this.
+
+        $user = NULL;
+
+        if (!empty($users))
+        {
+            foreach ($users as $key => $value)
+            {
+                if (isset($value['id']) && $value['id'] === $id)
+                {
+                    $user = $value;
+                }
+            }
+        }
+
+        if (!empty($user))
+        {
+            $this->set_response($user, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+        }
+        else
+        {
+            $this->set_response([
+                'status' => FALSE,
+                'message' => 'User could not be found'
+            ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+        }
+    }
+
+    public function users_post()
+    {
+        // $this->some_model->update_user( ... );
+        $message = [
+            'id' => 100, // Automatically generated by the model
+            'name' => $this->post('name'),
+            'email' => $this->post('email'),
+            'message' => 'Added a resource'
+        ];
+
+        $this->set_response($message, REST_Controller::HTTP_CREATED); // CREATED (201) being the HTTP response code
+    }
+
+    public function users_delete()
+    {
+        $id = (int) $this->get('id');
+
+        // Validate the id.
+        if ($id <= 0)
+        {
+            // Set the response and exit
+            $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        }
+
+        // $this->some_model->delete_something($id);
+        $message = [
+            'id' => $id,
+            'message' => 'Deleted the resource'
+        ];
+
+        $this->set_response($message, REST_Controller::HTTP_NO_CONTENT); // NO_CONTENT (204) being the HTTP response code
+    }
+
+    public function demo_get(){
+        $message = [
+            'nome' => 'Delta',
+            'Endereco' => 'Rua Lavras'
+        ];
+        $this->set_response($message,REST_Controller::HTTP_OK);
+    }
 }
