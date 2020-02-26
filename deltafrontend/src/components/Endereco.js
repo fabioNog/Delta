398122ebd135bae26952604aaad2bf5c1c9e2caa
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-26 14:24:31 -0300

    Criando Componente Endereço da minha lista de alunos

---
+++ deltafrontend/src/components/Endereco.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+//Importando Axios
+import axios from 'axios';
+
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: '#EB5160',
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+}))(TableRow);
+
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 600,
+  },
+});
+
+export default function CustomizedTables() {
+  const classes = useStyles();
+
+  const [data, setData] = useState([]);
+    useEffect(()=>{
+        axios.get('http://localhost/delta/deltabackend/index.php/api/aluno', function(req, res, next){
+            return res.sendStatus(200);
+         })
+        .then(res => setData(res.data))
+        .catch(res => console.log('erro'))
+    },[])
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Aluno</StyledTableCell>
+            <StyledTableCell>Numero</StyledTableCell>
+            <StyledTableCell>Rua</StyledTableCell>            
+            <StyledTableCell>Bairro</StyledTableCell>
+            <StyledTableCell>Cidade</StyledTableCell>
+            <StyledTableCell>Cep</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(row => (
+            <StyledTableRow key={row.id}>
+              <StyledTableCell>{row.a_nome}</StyledTableCell>
+              <StyledTableCell>{row.end_num}</StyledTableCell>
+              <StyledTableCell>{row.end_rua}</StyledTableCell>
+              <StyledTableCell>{row.end_bairro}</StyledTableCell>
+              <StyledTableCell>{row.end_cidade}</StyledTableCell>
+              <StyledTableCell>{row.end_cep}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
