749eb4db222b49fcc09a17fa4534d177d3f6afae
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-26 14:12:07 -0300

    Modificando estrutura do insert aluno para aceitar os campos da tabela endereço

--- deltabackend/application/controllers/API.php
+++ deltabackend/application/controllers/API.php
@@ -16,45 +16,35 @@ class Api extends REST_Controller{
         parent::__construct();
 
         $this->load->model('aluno_model');
+        $this->load->model('endereco_model');
     }
 
     //API - client sends id and on valid isbn aluno information is sent back
     function alunoByNome_get(){
-
-        $nome  = $this->get('a_nome');
-        
+        $nome  = $this->get('a_nome');        
         if(!$nome){
-
             $this->response("No Id specified", 400);
-
             exit;
         }
 
         $result = $this->aluno_model->getalunobynome($nome);
 
         if($result){
-
             $this->response($result, 200); 
-
             exit;
         } 
         else{
-
-             $this->response("Invalid ISBN", 404);
-
+             $this->response("Invalid ID", 404);
             exit;
         }
     } 
 
     //API -  Fetch All Alunos
     function aluno_get(){
-
         $result = $this->aluno_model->getallalunos();
 
         if($result){
-
             $this->response($result, 200); 
-
         } 
 
         else{
@@ -65,86 +55,74 @@ class Api extends REST_Controller{
     }
      
     //API - create a new aluno item in database.
-    function addAluno_post(){
-
-         $nome      = $this->post('a_nome');
+    function addAluno_post(){        
+        //Aluno
+         $nome = $this->post('a_nome');
+         $image = $this->post('image');
         
-         if(!$nome){
+        //Endereco
+        $end_aluno = $this->post('end_aluno');
+        $end_num = $this->post('end_num');
+        $end_rua = $this->post('end_rua');
+        $end_bairro = $this->post('end_bairro');
+        $end_cidade = $this->post('end_cidade');
+        $end_cep = $this->post('end_cep');
 
-                $this->response("Enter complete aluno information to save", 400);
-
-         }else{
-
-            $result = $this->aluno_model->add(array("a_nome"=>$nome));
-
-            if($result === 0){
-
-                $this->response("Aluno information coild not be saved. Try again.", 404);
-
-            }else{
-
-                $this->response("success", 200);  
-           
-            }
-
+        if(!$nome || !$image || !$end_aluno || !$end_num || !$end_rua || !$end_bairro || !$end_cidade || !$end_cep){
+        $this->response("Enter complete aluno information to save no banco", 400);
         }
-
+         else{
+            $aluno = $this->aluno_model->add(array("a_nome"=>$nome,"image"=>$image));
+            $endereco = $this->endereco_model->add(array(
+            "end_aluno"=>$end_aluno,
+            "end_num"=>$end_num,
+            "end_rua"=>$end_rua,
+            "end_bairro"=>$end_bairro,
+            "end_cidade"=>$end_cidade,
+            "end_cep"=>$end_cep));
+            if($aluno === 0 && $endereco){
+                $this->response("Aluno information coild not be saved. Try again.", 404);
+            }else{
+                $this->response("success", 200);             
+            }
+        }
     }
 
     
     //API - update a aluno 
     function updateAluno_put(){
          
-         $name      = $this->put('a_name');
+         $name = $this->put('a_name');
+         $image = $this->put('image');
+         $id = $this->put('id');
 
-         $id        = $this->put('id');
-         
          if(!$name){
-
-                $this->response("Enter complete aluno information to save", 400);
-
+            $this->response("Enter complete aluno information to save", 400);
          }else{
-            $result = $this->aluno_model->update($id, array("a_name"=>$name));
-
+            $result = $this->aluno_model->update($id, array("a_name"=>$name,"image"=>$image));
             if($result === 0){
-
                 $this->response("aluno information coild not be saved. Try again.", 404);
-
             }else{
-
                 $this->response("success", 200);  
-
             }
-
         }
-
     }
 
     //API - delete a aluno 
     function deleteAluno_delete()
     {
-
         $id  = $this->delete('id');
-
         if(!$id){
-
             $this->response("Parameter missing", 404);
-
-        }
-         
+        }         
         if($this->aluno_model->delete($id))
         {
-
             $this->response("Success", 200);
-
         } 
         else
         {
-
             $this->response("Failed", 400);
-
         }
-
     }
 
 
