d7166203535ff1cf2af04036f491ef5b45fa71c0
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-26 14:13:02 -0300

    Modificando Formulario Criar com os novos campos e adicionando função post de inserção nas tabelas

--- deltafrontend/src/components/FormularioCriar.js
+++ deltafrontend/src/components/FormularioCriar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -7,9 +7,12 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-//Import Component Avatar
+//Axios import
+import axios from 'axios';
+import qs  from 'qs'
+import Avatar from '@material-ui/core/Avatar';
 
-import Avatar from './Avatar'
+
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -28,11 +31,87 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(3, 0, 2),
     backgroundColor: '#EB5160',
   },
+
+  input: {
+    marginTop: theme.spacing(2),
+  },
+  avatar: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 export default function Cadastrar() {
   const classes = useStyles();
 
+  //States do Aluno
+  const [a_nome, setAnome] = useState('');
+  const [image, setImage] = useState('');
+  
+  //States do Endereco
+  const [end_aluno, setEndAluno] = useState(0);
+  const [end_num, setEndNum] = useState(0);
+  const [end_rua, setEndRua] = useState('');
+  const [end_bairro, setEndBairro] = useState('');
+  const [end_cidade, setEndCidade] = useState('');
+  const [end_cep, setEndCep] = useState('');
+  
+  
+  //Metodo upload Input
+  const upload = e =>{
+    let files = e.target.files[0]
+    let reader = new FileReader();
+    reader.readAsDataURL(files);
+    reader.onload = (e) => {              
+        setImage(e.target.result)
+    }
+  }
+
+  const handleNome = (e) =>{
+    return setAnome(e.target.value)
+  }
+
+  const handleEndAluno = (e) =>{
+    return setEndAluno(e.target.value)
+  }
+
+  const handleEndNum = (e) =>{
+    return setEndNum(e.target.value)
+  }
+
+  const handleEndRua = (e) =>{
+    return setEndRua(e.target.value)
+  }
+
+  const handleEndBairro = (e) =>{
+    return setEndBairro(e.target.value)
+  }
+  
+  const handleEndCidade = (e) =>{
+    return setEndCidade(e.target.value)
+  }
+  const handleEndCep = (e) =>{
+    return setEndCep(e.target.value)
+  }
+
+  const OnSubmit = (e) => {
+    e.preventDefault();    
+    const teste = {a_nome,image,end_aluno,end_num,end_rua,end_bairro,end_cidade,end_cep}
+    const QS = qs.stringify(teste)
+    console.log(QS)
+    const url = 'http://localhost/delta/deltabackend/index.php/api/addAluno'
+    const config = {
+     headers: {
+       'Content-Type': 'application/x-www-form-urlencoded'
+     }
+   }
+    axios.post(url, QS, config)
+   .then(function (response) {
+     console.log(response);
+   })
+   .catch(function (error) {
+     console.log(error);
+   });      
+}
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -40,11 +119,11 @@ export default function Cadastrar() {
         <Typography component="h1" variant="h4" style={{color: '#EB5160',}}>
           Cadastrar Aluno
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} noValidate onSubmit={OnSubmit} >
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
-                autoComplete="fname"
+                autoComplete="a_nome"
                 name="a_nome"
                 variant="outlined"
                 required
@@ -52,8 +131,11 @@ export default function Cadastrar() {
                 id="a_nome"
                 label="Nome"
                 autoFocus
+                value={a_nome}
+                onChange={handleNome}
               />
             </Grid>
+            
             <Grid item xs={12} sm={6}>
               <TextField
                 variant="outlined"
@@ -66,6 +148,7 @@ export default function Cadastrar() {
                 }}
                 id="end_aluno"
                 name="end_aluno"
+                onChange={handleEndAluno}                
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -78,8 +161,9 @@ export default function Cadastrar() {
                 InputLabelProps={{
                   shrink: true,
                 }}
-                id="numero"
-                name="numero"
+                id="end_num"
+                name="end_num"
+                onChange={handleEndNum}
               />
             </Grid>
             <Grid item xs={12}>
@@ -91,6 +175,7 @@ export default function Cadastrar() {
                 id="end_rua"
                 label="Rua"
                 autoFocus
+                onChange={handleEndRua}
               />
             </Grid>
             <Grid item xs={12}>
@@ -102,6 +187,7 @@ export default function Cadastrar() {
                 id="end_bairro"
                 label="Bairro"
                 autoFocus
+                onChange={handleEndBairro}
               />
             </Grid>
             <Grid item xs={12}>
@@ -113,6 +199,7 @@ export default function Cadastrar() {
                 id="end_cidade"
                 label="Cidade"
                 autoFocus
+                onChange={handleEndCidade}
               />
             </Grid>
             <Grid item xs={12}>
@@ -124,10 +211,23 @@ export default function Cadastrar() {
                 id="end_cep"
                 label="Cep"
                 autoFocus
+                onChange={handleEndCep}
               />
             </Grid>
-            <Grid item xs={12}>
-              <Avatar />
+            <Grid item xs={12} sm={2}>
+              <Avatar
+                variant={'rounded'}          
+                className={classes.input}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <input 
+                type="file" 
+                name={'image'}
+                className={classes.avatar}
+                onChange={upload}
+              >                
+              </input>
             </Grid>
           </Grid>
           <Button
@@ -135,7 +235,7 @@ export default function Cadastrar() {
             fullWidth
             variant="contained"
             color="primary"
-            className={classes.submit}
+            className={classes.submit}            
           >
             Cadastrar
           </Button>
